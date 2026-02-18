87c7ca73f2b1530375be218006bb476dddf3ea81
samuelvictorol <samuel.softdev@outlook.com>
2026-02-18 16:04:21 -0300

    teste rotas para seo

--- src/router/routes.js
+++ src/router/routes.js
@@ -6,10 +6,51 @@ const routes = [
       { path: '', component: () => import('src/pages/IndexPage.vue') },
       { path: 'catalogo', component: () => import('src/pages/CatalogoPage.vue') },
       { path: 'localizacao', component: () => import('src/pages/LocHorarioPage.vue') },
+
+      // Página base
       { path: 'servicos-manutencao', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+
+      // Rotas SEO (todas apontam para a mesma página)
+      { path: 'manutencao-lavadoras', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'manutencao-lavadoras-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'manutencao-de-lavadora', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'manutencao-de-lavadora-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+
+      { path: 'mangueiras-hidraulicas', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'mangueiras-hidraulicas-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'crimpagem-de-mangueiras', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'crimpagem-de-mangueiras-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+
+      { path: 'manutencao-bombas-dagua', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'manutencao-bombas-dagua-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'conserto-bomba-dagua', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'conserto-bomba-dagua-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+
+      { path: 'manutencao-compressores', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'manutencao-compressores-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+
+      { path: 'automacao-industrial', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'automacao-industrial-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+
+      { path: 'solda-tig', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'solda-mig', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'solda-tig-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'solda-mig-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+
+      { path: 'torno-mecanico', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'torno-mecanico-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+
+      { path: 'rebobinagem-de-motores', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'rebobinagem-de-motores-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+
+      { path: 'caminhao-munck', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'caminhao-munck-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+      { path: 'munck-valparaiso', component: () => import('src/pages/ServicosManutencaoPage.vue') },
+
       { path: 'catalogo/produto/:slug', name: 'produto', props: true, component: () => import('src/pages/ProdutoPage.vue') },
     ]
   },
+
   {
     path: '/admin',
     component: () => import('layouts/LandingLayout.vue'),
@@ -17,6 +58,7 @@ const routes = [
       { path: '', component: () => import('src/pages/admin/AdminPage.vue') },
     ]
   },
+
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
